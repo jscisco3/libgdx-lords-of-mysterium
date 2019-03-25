03f5dbf4e383a51c6ef51fb7271c9fd5a6080bd0
Jack Scisco <jscisco@redhat.com>
2019-03-24 20:30:41 -0400

    set width & height

--- desktop/src/com/jscisco/lom/desktop/DesktopLauncher.java
+++ desktop/src/com/jscisco/lom/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jscisco.lom.Game;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Game(), config);
+    }
 }
