3f9f8909d235bc1b009c58e6b39a19cc295510e3
Jack Scisco <jscisco@redhat.com>
2019-07-07 21:37:10 -0400

    NPCs default to resting

--- core/src/com/jscisco/lom/actor/NPC.java
+++ core/src/com/jscisco/lom/actor/NPC.java
@@ -2,9 +2,14 @@ package com.jscisco.lom.actor;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.jscisco.lom.commands.Command;
+import com.jscisco.lom.commands.RestCommand;
 import com.jscisco.lom.util.Position3D;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NPC extends Actor {
+
+    private final Logger logger = LoggerFactory.getLogger(NPC.class);
 
     public NPC(TextureRegion texture, Position3D position) {
         this.texture = texture;
@@ -13,6 +18,7 @@ public class NPC extends Actor {
 
     @Override
     public Command getNextCommand() {
-        return super.getNextCommand();
+        logger.info("{} is resting!", this);
+        return new RestCommand(null, this);
     }
 }
