5dc54fb4a5ccfab55b3452c5e1cd6c365c717d7d
Jack Scisco <jackscisco@gmail.com>
2019-07-11 09:07:59 -0400

    Give each entity a Dijkstra map for calculating path to a goal.
    WanderAI uses this Dijkstra map to find a path to a random point. Once
    it has reached the random point, it finds a new one and begins wandering
    towards that.
    
    AutoexploreState uses the Player's Dijkstra map to calculate the path.
    
    TODO: Switching between AI depending on needs.

--- core/src/com/jscisco/lom/attributes/ai/WanderAI.java
+++ core/src/com/jscisco/lom/attributes/ai/WanderAI.java
@@ -2,22 +2,57 @@ package com.jscisco.lom.attributes.ai;
 
 import com.jscisco.lom.action.Action;
 import com.jscisco.lom.action.MoveAction;
+import com.jscisco.lom.action.RestAction;
 import com.jscisco.lom.entity.Entity;
 import com.jscisco.lom.util.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import squidpony.squidmath.RNG;
+import squidpony.squidmath.Coord;
 
-public class WanderAI implements AI {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Choose a random, empty position on the map and then begin wandering towards it.
+ * Once the goal is reached, or becomes unreachable - choose a new goal.
+ */
+public class WanderAI extends AbstractAI {
 
     private static final Logger logger = LoggerFactory.getLogger(WanderAI.class);
 
-    RNG rng = new RNG();
+    private Position goal;
+
+    public WanderAI(Entity entity) {
+        super(entity);
+        this.goal = entity.getStage().findEmptyPosition();
+    }
 
     @Override
-    public Action nextAction(Entity entity) {
-        Position direction = new Position(rng.between(-1, 2), rng.between(-1, 2));
-        logger.debug("{} is wandering towards {}.", entity, direction);
-        return new MoveAction(entity, direction);
+    public Action nextAction() {
+        Position direction = calculateDirectionToWalk();
+        logger.debug("{} is wandering from {} towards {}.", this.entity, this.entity.getPosition(), goal);
+        if (direction == null) {
+            logger.debug("But the calculated direction was null");
+            return new RestAction(this.entity);
+        }
+        return new MoveAction(this.entity, direction);
     }
+
+    private Position calculateDirectionToWalk() {
+        Coord entityCoord = Coord.get(this.entity.getX(), this.entity.getY());
+        List<Coord> path = entity.getPathingMap().findPath(1,
+                new ArrayList<Coord>(),
+                new ArrayList<Coord>(),
+                entityCoord,
+                Coord.get(this.goal.getX(), this.goal.getY()));
+        if (!path.isEmpty() && !path.get(0).equals(entityCoord)) {
+            return new Position(path.get(0).x - entityCoord.x, path.get(0).y - entityCoord.y);
+        } else {
+            // No path, so find a new goal
+            this.goal = this.entity.getStage().findEmptyPosition();
+            this.entity.getPathingMap().clearGoals();
+            return null;
+        }
+    }
+
 }
