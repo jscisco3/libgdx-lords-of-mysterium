e6ff55782cb6f0437da67698c78e5938565cdd9a
Jack Scisco <jackscisco@gmail.com>
2019-07-14 19:45:14 -0400

    Initial Stats attribute work. This attribute currently contains generic stats but will likely contain more as they are developed (resistances, critical chance, etc.)

---
+++ core/src/com/jscisco/lom/attributes/Stats.java
@@ -0,0 +1,72 @@
+package com.jscisco.lom.attributes;
+
+public class Stats {
+
+    private int strength;
+    private int intelligence;
+    private int dexterity;
+    private int constitution;
+
+    private Stats() {
+
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public static class Builder {
+        private int strength;
+        private int intelligence;
+        private int dexterity;
+        private int constitution;
+
+        public Builder() {
+            this.strength = 0;
+            this.intelligence = 0;
+            this.dexterity = 0;
+            this.constitution = 0;
+        }
+
+        public Builder withStrength(int strength) {
+            this.strength = strength;
+            return this;
+        }
+
+        public Builder withIntelligence(int intelligence) {
+            this.intelligence = intelligence;
+            return this;
+        }
+
+        public Builder withDexterity(int dexterity) {
+            this.dexterity = dexterity;
+            return this;
+        }
+
+        public Builder withConstitution(int constitution) {
+            this.constitution = constitution;
+            return this;
+        }
+
+        public Stats build() {
+            Stats stats = new Stats();
+            stats.strength = this.strength;
+            stats.intelligence = this.intelligence;
+            stats.dexterity = this.dexterity;
+            stats.constitution = this.constitution;
+            return stats;
+        }
+    }
+}
