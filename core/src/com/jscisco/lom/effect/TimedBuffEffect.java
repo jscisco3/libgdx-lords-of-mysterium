974db30309787f52bdb73e436a3a5d4674686093
Jack Scisco <jscisco@redhat.com>
2019-12-23 12:34:37 -0500

    begin working on buffs

--- core/src/com/jscisco/lom/effect/TimedBuffEffect.java
+++ core/src/com/jscisco/lom/effect/TimedBuffEffect.java
@@ -1,8 +1,22 @@
 package com.jscisco.lom.effect;
 
+import com.jscisco.lom.entity.AbstractStat;
+import com.jscisco.lom.entity.Entity;
+
 public class TimedBuffEffect extends TimedEffect {
 
-    public TimedBuffEffect(int timer) {
+    public TimedBuffEffect(int timer, AbstractStat stat) {
         super(timer);
+    }
+
+    @Override
+    public void destroy() {
+        this.entity.ifPresent(e -> e.getStatistics());
+        super.destroy();
+    }
+
+    @Override
+    public void attach(Entity entity) {
+        super.attach(entity);
     }
 }
