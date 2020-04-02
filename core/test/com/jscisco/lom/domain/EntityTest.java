d4f32d7ffd4e7c4d5376c90fb002776fd336976b
Jack Scisco <jackscisco@gmail.com>
2020-04-01 22:03:23 -0400

    unit tests

--- core/test/com/jscisco/lom/domain/EntityTest.java
+++ core/test/com/jscisco/lom/domain/EntityTest.java
@@ -26,36 +26,77 @@ public class EntityTest {
     @Test
     public void testEntityWithHandlerHandlesAnEvent() {
         Entity e = EntityFactory.testEntity();
-        e.registerHandler(new EventHandler<EntityMovedEvent>() {
+        e.registerHandler(EntityMovedEvent.class, new EventHandler() {
             @Override
-            public void handle(EntityMovedEvent event) {
+            public void handle(Event event) {
                 e.position = new Position(10, 10);
             }
         });
 
-        Subject subject = new Subject() {
-            Observer o = null;
-
-            @Override
-            public void attach(Observer o) {
-                this.o = o;
-            }
-
-            @Override
-            public void detach(Observer o) {
-                this.o = null;
-            }
-
-            @Override
-            public void notifyObservers(Event event) {
-                this.o.update(event);
-            }
-        };
+        Subject subject = new SubjectImpl();
         subject.attach(e);
         subject.notifyObservers(new Event() {
         });
 
         Assertions.assertThat(e.position).isEqualTo(new Position(10, 10));
+    }
 
+    /**
+     * Given an entity
+     * That has multiple event handlers
+     * Only the appropriate handler should be called for a type of event
+     */
+    @Test
+    public void multipleDistinctEventHandlersAreNotCalledForSameEventType() {
+        Entity e = EntityFactory.testEntity();
+        e.registerHandler(EntityMovedEvent.class, new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                e.position = new Position(10, 10);
+            }
+        });
+        // Maybe these event handler should be mocks, since I want to verify their handle method was called and that is
+        // it.
+        e.registerHandler(EntityDiedEvent.class, new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                e.position = new Position(20, 20);
+            }
+        });
+        Subject subject = new SubjectImpl();
+        subject.attach(e);
+        subject.notifyObservers(new EntityMovedEvent(e, new Position(0, 0)));
+
+        Assertions.assertThat(e.position).isEqualTo(new Position(10, 10));
+    }
+
+    /**
+     * Given an Entity
+     * That has multiple event handlers each of which handle the same event
+     * They should all act when notified of a particular event.
+     */
+    // TODO
+    @Test
+    public void multipleEventHandlersSameTypeCalledForEventOfThatType() {
+
+    }
+
+    private class SubjectImpl implements Subject {
+        Observer o = null;
+
+        @Override
+        public void attach(Observer o) {
+            this.o = o;
+        }
+
+        @Override
+        public void detach(Observer o) {
+            this.o = null;
+        }
+
+        @Override
+        public void notifyObservers(Event event) {
+            this.o.update(event);
+        }
     }
 }
--- core/test/com/jscisco/lom/domain/TestEntity.java
+++ core/test/com/jscisco/lom/domain/TestEntity.java
@@ -2,6 +2,6 @@ package com.jscisco.lom.domain;
 
 public class TestEntity extends Entity {
     public TestEntity(EntityId id, EntityName name) {
-        super(id, name);
+        super(id, name, new Position(0, 0));
     }
 }
