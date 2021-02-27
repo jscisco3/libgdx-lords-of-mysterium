4060432a0fadb3a1ba3de78851d1a393691999e8
Jack Scisco <jscisco@redhat.com>
2021-02-27 09:39:51 -0500

    basic player ui: name & position

--- core/src/main/java/com/jscisco/lom/application/GameScreen.java
+++ core/src/main/java/com/jscisco/lom/application/GameScreen.java
@@ -3,11 +3,18 @@ package com.jscisco.lom.application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.jscisco.lom.Game;
 import com.jscisco.lom.domain.Direction;
 import com.jscisco.lom.domain.Position;
@@ -26,33 +33,41 @@ public class GameScreen extends AbstractScreen {
 
     private OrthographicCamera camera;
 
-    Stage stage = new Stage();
+    Stage stage;
     Player player = ActorFactory.player();
     AdventureInputProcessor processor;
     private InputMultiplexer inputMultiplexer = new InputMultiplexer();
     Level level;
 
     // UI Elements
-    Label cameraPosition;
+    private AdventurerUI adventurerUI;
 
-    float cameraWidth = 1280;
-    float cameraHeight = 900;
+    int cameraWidth = GameConfiguration.SCREEN_WIDTH;
+    int cameraHeight = GameConfiguration.SCREEN_HEIGHT;
+
+    Vector3 offset = new Vector3(200f, 0f, 0f);
+
+    Matrix4 transform = new Matrix4(offset, new Quaternion(), new Vector3(1f, 1f, 1f));
 
     public GameScreen(Game game) {
         super(game);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, cameraWidth, cameraHeight);
         camera.update();
-        Skin skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
-        cameraPosition = new Label("Camera Position", skin);
+        stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         processor = new AdventureInputProcessor(player);
         // Create a zone and a stage
         level = new Level();
         level.addEntityAtPosition(player, Position.of(1, 1));
         inputMultiplexer.addProcessor(processor);
         inputMultiplexer.addProcessor(stage);
-        stage.addActor(cameraPosition);
-        cameraPosition.setPosition(0, stage.getHeight() - 50f);
+
+        adventurerUI = new AdventurerUI(player, 0, 0, offset.x, cameraHeight, Color.GRAY);
+        adventurerUI.setWidth(offset.x);
+        adventurerUI.setHeight(Gdx.graphics.getHeight());
+        adventurerUI.top();
+        stage.addActor(adventurerUI);
+        stage.setDebugAll(false);
     }
 
     @Override
@@ -67,22 +82,20 @@ public class GameScreen extends AbstractScreen {
         handleInput();
         level.process();
         updateCamera();
+        batch.setTransformMatrix(transform);
         level.draw(batch, camera);
         stage.draw();
-//        batch.begin();
-//        batch.end();
     }
 
     private void updateCamera() {
         float x = player.getPosition().getX() * 24;
         float y = player.getPosition().getY() * 24;
 
-        x = Math.min(Math.max(cameraWidth / 2f - 200f, x), level.getWidth() * 24f - (cameraWidth / 2f));
-        y = Math.min(Math.max(cameraHeight / 2f, y), level.getHeight() * 24f - cameraHeight / 2f);
+        x = Math.min(Math.max(cameraWidth / 2f, x), level.getWidth() * 24f - (cameraWidth / 2f) + offset.x);
+        y = Math.min(Math.max(cameraHeight / 2f, y), level.getHeight() * 24f - cameraHeight / 2f + offset.y);
 
         // Clamp x and y
         camera.position.set(x, y, 0);
-        cameraPosition.setText(String.format("Camera position: (%f, %f)", x, y));
         camera.update();
     }
 
