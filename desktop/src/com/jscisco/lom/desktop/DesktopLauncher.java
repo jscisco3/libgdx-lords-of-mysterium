f89bda3896b7763e2593c3894d2e175b8480e607
Jack Scisco <jackscisco@gmail.com>
2021-06-22 19:25:17 -0400

    update texture path to work on windows build

--- desktop/src/com/jscisco/lom/desktop/DesktopLauncher.java
+++ desktop/src/com/jscisco/lom/desktop/DesktopLauncher.java
@@ -5,8 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.jscisco.lom.Game;
 import com.jscisco.lom.application.configuration.GameConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DesktopLauncher {
+
+    private static final Logger logger = LoggerFactory.getLogger(DesktopLauncher.class);
+
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Lords of Mysterium";
@@ -17,7 +25,8 @@ public class DesktopLauncher {
         settings.maxWidth = 2048;
         settings.maxHeight = 2048;
 
-        TexturePacker.process(settings, "../../assets/textures/", "packed/", "assets");
+        Path textureAssets = Paths.get(".", "assets", "textures");
+        TexturePacker.process(settings, textureAssets.toAbsolutePath().toString(), "packed/", "assets");
 
         // Unlock FPS
         config.backgroundFPS = 10;
